b910970f82f8e591290fb3e876e8773a7b7c8295
Justin-Time319 <98084993+Justin-Time319@users.noreply.github.com>
2025-10-19 02:07:48 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,13 @@
+// next.config.ts
 import type { NextConfig } from "next";
+
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "cdn.discordapp.com" },
-      { protocol: "https", hostname: "media.discordapp.net" }
+      { protocol: "https", hostname: "media.discordapp.net" },
     ],
   },
 };
+
 export default nextConfig;
